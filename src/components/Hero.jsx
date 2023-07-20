40ee2cafd7abaa54e3890a25e9537b2ed5d68d71
Joe <gozefatef1999@gmail.com>
2023-07-20 19:00:15 +0300

    edit ui

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,39 @@
 import React from 'react'
 import { styles } from '../styles'
+import { motion } from 'framer-motion'
 import ComputersCanvas from './canvas/Computers'
 
 const Hero = () => {
   return (
     <section className='mx-auto relative w-full h-screen'>
-      <div className={`${styles.paddingX} absolute mx-auto inset-0 top-[120px] flex flex-row items-start gap-5`}>
+      <div className={`${styles.paddingX} absolute mx-auto inset-0 top-[120px] flex flex-row items-start gap-5 className='sm:mb-0 mb-30'`}>
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
           <div className='w-1 sm:h-80 h-40 violet-gradient'/>
         </div>
         <div >
-          <h1 className={`${styles.heroHeadText}`}>Hi, I'm <span className='text-[#915eff]'>Joe Erian</span></h1>
+          <h1 className={`${styles.heroHeadText} text-[30px]`}>Hi, I'm <span className='text-[#915eff]'>Joe Erian</span></h1>
           <h2 className={`${styles.heroSubText} mt-2 text-white-100`}>Full Stack Web Developer <br className='sm:block hidden '/> Graphic Designer</h2>
         </div>
       </div>
-        <ComputersCanvas />
+      <ComputersCanvas />
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[30px] h-[45px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div 
+              animate= {{
+                y:[0,10,0]
+              }}
+              transition={{
+                duration: 1.2,
+                repeat: Infinity,
+                repeatType: 'loop'
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+      </div>
     </section>
   )
 }
