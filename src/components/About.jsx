ac09e9ec71efb940c97a3751a0beb4f52fb13553
Joe <gozefatef1999@gmail.com>
2023-07-21 17:35:34 +0300

    finished about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,41 +1,52 @@
-import React from 'react'
-import { services } from '../constants'
-import { styles } from '../styles'
-import { Tilt } from 'react-tilt'
+import React from 'react';
+import { services } from '../constants';
+import { styles } from '../styles';
+import Tilt from 'react-tilt';
+import { motion } from 'framer-motion';
+import {SectionWrapper} from '../hoc/'
+import {textVariant, fadeIn} from '../utils/motion'
+
+export const ServiceCard = ({title, icon, index}) =>{
+  return (
+        <Tilt className="xs:w-[250px] w-full">
+            <motion.div 
+              variants={fadeIn('right','spring', 0.5 * index, 0.75)}
+              className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+            >
+              <div
+                options={{
+                  max: 45,
+                  scale: 1,
+                  speed: 450
+                }}
+                className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex flex-col items-center justify-evenly'
+              >
+                <img src={icon} alt={title} className='w-16 h-16 object-contain'/>
+                <h2 className='text-white text-[20px] text-center'>{title}</h2>
+              </div>
+            </motion.div>
+          </Tilt>
+  )
+}
+
 
 const About = () => {
-  const defaultOptions = {
-    reverse:        false,  // reverse the tilt direction
-    max:            35,     // max tilt rotation (degrees)
-    perspective:    1000,   // Transform perspective, the lower the more extreme the tilt gets.
-    scale:          1.1,    // 2 = 200%, 1.5 = 150%, etc..
-    speed:          1000,   // Speed of the enter/exit transition
-    transition:     true,   // Set a transition on enter/exit.
-    axis:           null,   // What axis should be disabled. Can be X or Y.
-    reset:          true,    // If the tilt effect has to be reset on exit.
-    easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
-  }
   return (
-    <div>
-      <div>
+    <div className={`mt-10`}>
+      <motion.div variants={textVariant()}>
         <h3 className={`${styles.sectionSubText}`}>Introduction</h3>
         <h1 className={`${styles.sectionHeadText}`}>Overview.</h1>
-        <p className={`${styles.heroSubText}`}></p>
-      </div>
-      <div className='flex flex-wrap flex-row w-full'>
-        {services.map((service)=>{
-          <div>
-          <Tilt options={defaultOptions} style={{height: 250, width: 250}}>
-            <div>
-              <img src={service.icon}/>
-              <h2>{service.title}</h2>
-            </div>
-          </Tilt>
-          </div>
-        })} 
+      </motion.div>
+      <motion.p variants={fadeIn('','',0.1,1)} className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
+          Hello Boys Im gonna show you the hell of  a night in my club
+      </motion.p>
+      <div className='mt-20 flex flex-wrap gap-10 flex-row w-full'>
+        {services.map((service, index)=>(
+          <ServiceCard key={service.title} index={index} {...service}/>
+        ))} 
       </div>
     </div>
   )
 }
 
-export default About
+export default SectionWrapper(About, 'about')
