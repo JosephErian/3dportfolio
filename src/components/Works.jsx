7d6051f7f0f0984529607e1916e87a271574c609
Joe <gozefatef1999@gmail.com>
2023-07-23 18:42:29 +0300

    works editing

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -9,8 +9,9 @@ import { fadeIn, textVariant } from '../utils/motion';
 
 const ProjectCard = ({index, name,description, tags, image, source_code_link }) => {
   return (
-    <a href={source_code_link}>
+    //The start of the projectCard
     <motion.div variants={fadeIn('up','spring', index * 0.5, 0.75)} >
+      {/*The props of the tilt*/}
       <Tilt
       className='bg-tertiary p-5 w-full rounded-2xl sm:w-[360px]'
         options={{
@@ -19,22 +20,31 @@ const ProjectCard = ({index, name,description, tags, image, source_code_link }) 
           speed: 450,
         }} 
         >
+          {/*The props of the image object and others*/} 
         <div className='relative w-full h-[230px]'>
           <img className='object-cover w-full h-full rounded-2xl' src={image} alt={name}/>
           <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
             <div 
-            onClick={()=> window.open(source_code_link, '_blank')}
-            className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
-            >
-              <img src={github} alt='github' className='w-1/2 h-1/2 object-contain'/>
+            /*that how to set a link*/
+              onClick={()=> window.open(source_code_link, '_blank')}
+              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+              >
+                <img src={github} alt='github' className='w-1/2 h-1/2 object-contain'/>
             </div>
-            {/* <h2>{name}</h2>
-            <p>{description}</p>  */}
+          
           </div>
+        </div>
+        <div className='mt-5'>
+            <h2 className='text-white font-bold text-[24px]'>{name}</h2>
+            <p className='text-secondary text-[14px]'>{description}</p> 
+        </div>
+        <div className='mt-4 flex flex-wrap gap-2'>
+          {tags.map(tag =>(
+            <p key={tag.name} className={`text-[14px] ${tag.color}`}>#{tag.name}</p>
+          ))}
         </div>
       </Tilt>
     </motion.div>
-  </a>
   )
 }
 
@@ -54,9 +64,9 @@ const Works = () => {
         ducimus debitis repellendus voluptates, 
         aspernatur officia omnis.
       </motion.p>
-      <div className='flex flex-row flex-wrap gap-14 justify-center'>
+      <div className='flex flex-row flex-wrap gap-8 justify-center mt-5'>
           {projects.map((project, index)=>(
-            <ProjectCard key={index} className='justify-center' {...project}/>
+            <ProjectCard key={index} {...project}/>
           
           ))}
       </div>
